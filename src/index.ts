ce87e2e4fcaf8e4776f141f19eb46d5c4fb66e16
isahszz <isabela.mariano@estudante.ifms.edu.br>
2025-09-15 10:05:41 -0300

    Atualização de script de start para utilizar o mongodb

--- src/index.ts
+++ src/index.ts
@@ -47,4 +47,21 @@ app.listen(8000, () => {
     console.log('Server is running on port 8000');
 });
 
+//Tarefa . Criar uma rota get para produtos que retorne a lista de produtos do banco de dados.
+//O produto deve ter id, nome preco,urlfoto,descricao
+//Deve se criar uma tabela no banco de dados AIVEN para armazenar os produtos
+//A resposta deve ser um array de produtos em formato JSON
+//Crie  o codigo sql para criar a tabela de produtos
+/*
+CREATE TABLE produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    urlfoto VARCHAR(255), NOT NULL
+    descricao TEXT
+);
+Faz pelo menos 3 inserçoes nessa tabela 
+*/  
+   
+
 
